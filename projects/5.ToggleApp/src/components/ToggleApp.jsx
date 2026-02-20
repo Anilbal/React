df2f8b1186aefd04cb6f0a99f1b3126ddea6767a
Anil Bal <anilbal7676@gmail.com>
2026-02-20 16:30:02 +0545

    step 3 add

--- projects/5.ToggleApp/src/components/ToggleApp.jsx
+++ projects/5.ToggleApp/src/components/ToggleApp.jsx
@@ -2,6 +2,7 @@ export function ToggleApp() {
   return (
     <div className="toggle-container">
       <button id="toggle-button">Message</button>
+      <p id="message">I love coding app!</p>
     </div>
   );
 }
