542482640411a271ef03b8ba5c8f85ddb2c7cce9
Anil Bal <anilbal7676@gmail.com>
2026-02-20 16:35:46 +0545

    step 7 add

--- projects/5.ToggleApp/src/components/ToggleApp.jsx
+++ projects/5.ToggleApp/src/components/ToggleApp.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export function ToggleApp() {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <div className="toggle-container">
       <button id="toggle-button">Message</button>
