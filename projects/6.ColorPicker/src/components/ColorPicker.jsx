3153287eefbae9a295d5bef7b845ed961633781a
Anil Bal <anilbal7676@gmail.com>
2026-02-21 13:20:07 +0545

    step 4 add

--- projects/6.ColorPicker/src/components/ColorPicker.jsx
+++ projects/6.ColorPicker/src/components/ColorPicker.jsx
@@ -2,5 +2,9 @@ import { useState } from "react";
 
 export const ColorPicker = () => {
   const [color, setColor] = useState("#ffffff");
-  return <div id="color-picker-container"></div>;
+  return (
+    <div id="color-picker-container">
+      <input type="color" id="color-input"/>
+    </div>
+  );
 };
