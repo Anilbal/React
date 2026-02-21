f28801c47760c1ada46c136682301438fe534dce
Anil Bal <anilbal7676@gmail.com>
2026-02-21 13:31:08 +0545

    project complete

--- projects/6.ColorPicker/src/components/ColorPicker.jsx
+++ projects/6.ColorPicker/src/components/ColorPicker.jsx
@@ -3,10 +3,15 @@ import { useState } from "react";
 export const ColorPicker = () => {
   const [color, setColor] = useState("#ffffff");
   const handleColorChange = (e) => {
-    console.log(e.target);
+    setColor(e.target.value);
   };
   return (
-    <div id="color-picker-container">
+    <div
+      id="color-picker-container"
+      style={{
+        backgroundColor: `${color}`,
+      }}
+    >
       <input
         type="color"
         id="color-input"
