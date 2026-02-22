457705d0b653ef3fa5e2ab4b97748c22785fad97
Anil Bal <anilbal7676@gmail.com>
2026-02-22 20:26:01 +0545

    step 6 add

--- projects/7.FruitsSearch/src/components/FruitsSearch.jsx
+++ projects/7.FruitsSearch/src/components/FruitsSearch.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 export function FruitsSearch() {
-  const [query,setQuery]=useState("")
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
   return (
     <div id="search-container">
       <form>
