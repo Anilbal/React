1b0d108279d3e5636adca5002fc666bc39e8d27e
Anil Bal <anilbal7676@gmail.com>
2026-02-23 13:01:04 +0545

    step 7 add

--- projects/8.PasswordGenerator/src/components/OtpGenerator.jsx
+++ projects/8.PasswordGenerator/src/components/OtpGenerator.jsx
@@ -7,6 +7,7 @@ export const OtpGenerator = () => {
       <h1 id="otp-title">OTP Generator</h1>
       <h2 id="otp-display">Click 'Generate OTP' to get a code</h2>
       <p id="otp-timer" aria-live="assertive"></p>
+      <button>Generate OTP</button>
     </div>
   );
 };
