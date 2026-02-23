d3efbbc44214fd8e64e29e4ac641048a1c4fa55c
Anil Bal <anilbal7676@gmail.com>
2026-02-23 12:59:26 +0545

    step 6 add

--- projects/8.PasswordGenerator/src/components/OtpGenerator.jsx
+++ projects/8.PasswordGenerator/src/components/OtpGenerator.jsx
@@ -4,8 +4,9 @@ export const OtpGenerator = () => {
   useEffect(() => {}, []);
   return (
     <div className="container">
-      <h1 id="otp-title">Otp Generator</h1>
+      <h1 id="otp-title">OTP Generator</h1>
       <h2 id="otp-display">Click 'Generate OTP' to get a code</h2>
+      <p id="otp-timer" aria-live="assertive"></p>
     </div>
   );
 };
