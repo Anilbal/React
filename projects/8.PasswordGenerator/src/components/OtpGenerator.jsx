2a16fd4ede1fb62b5801cbf1d3c68fc634d99714
Anil Bal <anilbal7676@gmail.com>
2026-02-23 13:36:04 +0545

    step 8 add

--- projects/8.PasswordGenerator/src/components/OtpGenerator.jsx
+++ projects/8.PasswordGenerator/src/components/OtpGenerator.jsx
@@ -1,13 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const OtpGenerator = () => {
+  const [otp, setOtp] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(0);
+  const timerRef = useRef(null);
+
   useEffect(() => {}, []);
+  
   return (
     <div className="container">
       <h1 id="otp-title">OTP Generator</h1>
       <h2 id="otp-display">Click 'Generate OTP' to get a code</h2>
-      <p id="otp-timer" aria-live="assertive"></p>
-      <button>Generate OTP</button>
+      <p id="otp-timer" aria-live="polite"></p>
+      <button id="generate-otp-button">Generate OTP</button>
     </div>
   );
 };
