a75c471cdfeb7be3f9c356bdadeb9d3ddae4f1b4
Anil Bal <anilbal7676@gmail.com>
2026-02-24 20:34:02 +0545

    step 4 add

--- projects/9.SuperHeroApplicationForm/src/components/SuperHeroForm.jsx
+++ projects/9.SuperHeroApplicationForm/src/components/SuperHeroForm.jsx
@@ -9,6 +9,9 @@ export const SuperHeroForm = () => {
     <div className="form-wrap">
       <h2>Superhero Application Form</h2>
       <p>Please complete all fields</p>
+      <form>
+        <div className="section"></div>
+      </form>
     </div>
   );
 };
