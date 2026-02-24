4207e022850637feea258c4d9c1204fa5cc66f70
Anil Bal <anilbal7676@gmail.com>
2026-02-24 20:30:17 +0545

    step 2 add

--- projects/9.SuperHeroApplicationForm/src/components/SuperHeroForm.jsx
+++ projects/9.SuperHeroApplicationForm/src/components/SuperHeroForm.jsx
@@ -1,8 +1,12 @@
-export const SuperHeroForm=()=>{
-    return (
-      <div className="form-wrap">
-        <h2>Superhero Application Form</h2>
-        <p>Please complete all fields</p>
-      </div>
-    );
-}
+import { useState } from "react";
+
+export const SuperHeroForm = () => {
+  const [heroName, setHeroName] = useState("");
+  const [realName, setRealName] = useState("");
+  return (
+    <div className="form-wrap">
+      <h2>Superhero Application Form</h2>
+      <p>Please complete all fields</p>
+    </div>
+  );
+};
